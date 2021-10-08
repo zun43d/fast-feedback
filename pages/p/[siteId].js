78db1ddf53b891febc7c1b5ae5bb264ea376e093
Zunaed Ahmed <zunaedahmed.info@gmail.com>
2021-10-08 16:26:38 +0600

    Comment box added

--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -1,7 +1,8 @@
-import { useRouter } from 'next/router';
-import { Box } from '@chakra-ui/core';
+import { useRef } from 'react';
+import { Box, Button, FormControl, FormLabel, Input } from '@chakra-ui/core';
 
 import { getAllFeedback, getAllSites } from '../../lib/db-admin';
+import { useAuth } from '@/lib/auth';
 
 export async function getStaticProps(context) {
 	const siteId = context.params.siteId;
@@ -29,8 +30,37 @@ export async function getStaticPaths() {
 	};
 }
 
-export default function FeedbackPage() {
-	const router = useRouter();
+export default function FeedbackPage({ initialFeedback }) {
+	const auth = useAuth();
+	const inputEl = useRef(null);
 
-	return <Box>Page ID: {router.query.siteId}</Box>;
+	return (
+		<Box
+			display="flex"
+			flexDirection="column"
+			width="full"
+			maxWidth="700px"
+			margin="0 auto"
+		>
+			{auth.user && (
+				<Box as="form">
+					<FormControl my={8}>
+						<FormLabel htmlFor="comment">Comment</FormLabel>
+						<Input
+							ref={inputEl}
+							id="comment"
+							placeholder="Leave a comment..."
+						/>
+						<Button mt={4} type="submit" fontWeight="medium">
+							Add Comment
+						</Button>
+					</FormControl>
+				</Box>
+			)}
+			{initialFeedback &&
+				initialFeedback.map((feedback) => {
+					<Feedback key={feedback.id} {...feedback} />;
+				})}
+		</Box>
+	);
 }
