ed5d5a8527d22ec5b9f68430e8f1c8cf6e5e0fb1
Zunaed Ahmed <zunaedahmed.info@gmail.com>
2021-10-08 16:28:23 +0600

    Feedback component added to `[siteId].js`

--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -3,6 +3,7 @@ import { Box, Button, FormControl, FormLabel, Input } from '@chakra-ui/core';
 
 import { getAllFeedback, getAllSites } from '../../lib/db-admin';
 import { useAuth } from '@/lib/auth';
+import Feedback from '@/components/feedback';
 
 export async function getStaticProps(context) {
 	const siteId = context.params.siteId;
